315625f7372ccecd17d2a817b1563a33ebd772cf
Luis Rivas Lopez <76708966+Lrivaslo5@users.noreply.github.com>
2022-05-15 23:41:44 -0400

    Footer Banner Off

--- ecommerce-store/components/Footerbanner.jsx
+++ ecommerce-store/components/Footerbanner.jsx
@@ -2,16 +2,16 @@ import React from 'react';
 import Link from 'next/link';
 import { urlFor } from '../lib/client';
 
-const Footerbanner = ({ footerBanner: {discount, largeText1, largeText2, saleTime, smallText, midText, product, buttonText, image
-}   }) => {
+const Footerbanner = ({ footerBanner}) => {
   return (
     <div className="footer-banner-container">
       <div className="banner-desc">
         <div className="left">
-          <p>{discount}</p>
+          FooterBanner
+          {/* <p>{discount}</p>
           <h3>{largeText1}</h3>
           <h3>{largeText2}</h3>
-          <p>{saleTime}</p>
+          <p>{saleTime}</p> */}
         </div>
         <div className="right">
         </div>
@@ -21,3 +21,5 @@ const Footerbanner = ({ footerBanner: {discount, largeText1, largeText2, saleTim
 }
 
 export default Footerbanner
+
+// : {discount, largeText1, largeText2, saleTime, smallText, midText, product, buttonText, image}
