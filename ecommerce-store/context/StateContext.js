d62f37c4cff36c27bf6dfd863c5b43c6cb19444a
Luis Rivas Lopez <76708966+Lrivaslo5@users.noreply.github.com>
2022-05-18 18:28:20 -0400

    Configure Cart adding

--- ecommerce-store/context/StateContext.js
+++ ecommerce-store/context/StateContext.js
@@ -10,6 +10,34 @@ export const StateContext = ({ children }) => {
   const [totalQuantities, setTotalQuantities] = useState(0);
   const [qty, setQty] = useState(1);
 
+  //Takes in product and quantity, updates total price based on these values
+  const onAdd = (product, quantity) => {
+    const checkProductInCart = cartItems.find(
+      (item) => item._id === product._id
+    );
+    if (checkProductInCart) {
+      setTotalPrice(
+        (prevTotalPrice) => prevTotalPrice + product.price * quantity
+      );
+      setTotalQuantities(
+        (prevTotalQuantities) => prevTotalQuantities + quantity
+      );
+
+      const updatedCartItems = cartItems.map((cartProduct) => {
+        if (cartProduct._id === product._id)
+          return {
+            ...cartProduct, //spread cart product
+            quantity: cartProduct.quantity + quantity,
+          };
+      });
+      setCartItems(updatedCartItems);
+    } else {
+      product.quantity = quantity;
+      setCartItems([...cartItems, { ...product }]);
+    }
+    toast.success(`${qty} ${product.name} added to cart.`);
+  };
+
   const incQty = () => {
     setQty((prevQty) => prevQty + 1);
   };
@@ -32,6 +60,7 @@ export const StateContext = ({ children }) => {
         qty,
         incQty,
         decQty,
+        onAdd,
       }}
     >
       {children}
--- ecommerce-store/pages/_app.js
+++ ecommerce-store/pages/_app.js
@@ -1,11 +1,11 @@
 import React from "react";
 import { Toaster } from "react-hot-toast";
+//this allows for notification to pop up
 
 import { Layout } from "../components";
 import "../styles/globals.css";
 import { StateContext } from "../context/StateContext";
 
-//
 function MyApp({ Component, pageProps }) {
   return (
     <StateContext>
