06e0bd5257e2691f4739f338dca3e4ba1b44221b
Luis Rivas Lopez <76708966+Lrivaslo5@users.noreply.github.com>
2022-05-18 22:17:20 -0400

    fixed previous bugs, one cart image issue left

--- ecommerce-store/context/StateContext.js
+++ ecommerce-store/context/StateContext.js
@@ -10,19 +10,21 @@ export const StateContext = ({ children }) => {
   const [totalQuantities, setTotalQuantities] = useState(0);
   const [qty, setQty] = useState(1);
 
+  let foundProduct; //this allows us determine what product we are working with
+  let index;
+
   //Takes in product and quantity, updates total price based on these values
   const onAdd = (product, quantity) => {
     const checkProductInCart = cartItems.find(
       (item) => item._id === product._id
     );
-    if (checkProductInCart) {
-      setTotalPrice(
-        (prevTotalPrice) => prevTotalPrice + product.price * quantity
-      );
-      setTotalQuantities(
-        (prevTotalQuantities) => prevTotalQuantities + quantity
-      );
 
+    setTotalPrice(
+      (prevTotalPrice) => prevTotalPrice + product.price * quantity
+    );
+    setTotalQuantities((prevTotalQuantities) => prevTotalQuantities + quantity);
+
+    if (checkProductInCart) {
       const updatedCartItems = cartItems.map((cartProduct) => {
         if (cartProduct._id === product._id)
           return {
@@ -50,10 +52,40 @@ export const StateContext = ({ children }) => {
     });
   };
 
+  // accepts id and value of our product we are working with, will allow us to add/subtract product quantities in cart
+  const toggleCartItemQuantity = (id, value) => {
+    foundProduct = cartItems.find((item) => item._id === id); //we can select product we want
+    index = cartItems.findIndex((product) => product._id === id); // we identify selected product index here
+    const newCartItems = cartItems.filter((item) => item._id !== id);
+    // splice removes old items and adds updated product items.. but is mutative (nono), so we use filter instead
+
+    if (value === "inc") {
+      setCartItems([
+        ...newCartItems, // only new items
+        { ...foundProduct, quantity: foundProduct.quantity + 1 },
+      ]);
+      // foundProduct.quantity += 1;
+      // cartItems[index] = foundProduct; //do not mutate state, use setCart items instead
+      setTotalPrice((prevTotalPrice) => prevTotalPrice + foundProduct.price);
+      setTotalQuantities((prevTotalQuantities) => prevTotalQuantities + 1);
+    } else if (value === "dec") {
+      if (foundProduct.quantity > 1) {
+        // can't lower las than 0
+        setCartItems([
+          ...newCartItems,
+          { ...foundProduct, quantity: foundProduct.quantity - 1 },
+        ]);
+        setTotalPrice((prevTotalPrice) => prevTotalPrice - foundProduct.price);
+        setTotalQuantities((prevTotalQuantities) => prevTotalQuantities - 1);
+      }
+    }
+  };
+
   return (
     <Context.Provider
       value={{
         showCart,
+        setShowCart,
         cartItems,
         totalPrice,
         totalQuantities,
@@ -61,6 +93,7 @@ export const StateContext = ({ children }) => {
         incQty,
         decQty,
         onAdd,
+        toggleCartItemQuantity,
       }}
     >
       {children}
