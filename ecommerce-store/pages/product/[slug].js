a417ec4bd1fce52b2d45fad9ebe2194dc9398010
Luis Rivas Lopez <76708966+Lrivaslo5@users.noreply.github.com>
2022-05-23 21:26:35 -0400

    Finalize fixing some bugs

--- ecommerce-store/pages/product/[slug].js
+++ ecommerce-store/pages/product/[slug].js
@@ -19,7 +19,11 @@ const ProductDetails = ({ product, products }) => {
   const { image, name, details, price } = product;
 
   const [index, setIndex] = useState(0);
-  const { decQty, incQty, qty, onAdd } = useStateContext(); // Calling exported state context to hook data for website logic
+  const { decQty, incQty, qty, onAdd, setShowCart } = useStateContext(); // Calling exported state context to hook data for website logic
+  const handleBuyNow = () => {
+    onAdd(product, qty);
+    setShowCart(true);
+  };
 
   return (
     <div>
@@ -68,9 +72,7 @@ const ProductDetails = ({ product, products }) => {
               <span className="minus" onClick={decQty}>
                 <AiOutlineMinus />
               </span>
-              <span className="num" onClick="">
-                {qty}
-              </span>
+              <span className="num">{qty}</span>
               <span className="plus" onClick={incQty}>
                 <AiOutlinePlus />
               </span>
@@ -84,7 +86,7 @@ const ProductDetails = ({ product, products }) => {
             >
               Add to Cart
             </button>
-            <button type="button" className="buy-now" onClick="">
+            <button type="button" className="buy-now" onClick={handleBuyNow}>
               Buy Now
             </button>
           </div>
